341c34d46cdb0cbdd2d5f66632e9fcefb1cdaf91
Userok1 <kletkadlyakrabov@gmail.com>
2024-03-25 14:24:18 +0300

    practica2

---
+++ practica2/src/practica2/Main.java
@@ -0,0 +1,9 @@
+package practica2;
+
+public class Main {
+    public static void main(String[] args) {
+        Ball b = new Ball("Basketball", 750, 650);
+        System.out.println(b.getName());
+        System.out.println(b.toString());
+    }
+}
