5acc5558ac15e0da1fd6f257b768a940e0c88630
Userok1 <kletkadlyakrabov@gmail.com>
2024-03-26 19:07:24 +0300

    pr2_5

---
+++ practica2/pr2_5/src/pr2_5/Main.java
@@ -0,0 +1,30 @@
+package pr2_5;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = -1;
+        while (n < 0) {
+            System.out.print("Введите число, факториал которого хотите найти: ");
+            if (scanner.hasNextInt()) {
+                n = scanner.nextInt();
+            }
+        }
+        if (n == 0) {
+            System.out.println(1);
+        } else {
+            int result = fact(n);
+            System.out.println("Factorial("+n+") = "+result);
+        }
+    }
+
+    public static int fact(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
